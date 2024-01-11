52c83622fb5511e124549a413de2f33af48bbe62
ArthurDeWitt <124618265+ArthurDeWitt@users.noreply.github.com>
2024-01-11 23:24:21 +0300

    Update $ and total

--- src/com/teachmeskills/mycourse/Runner.java
+++ src/com/teachmeskills/mycourse/Runner.java
@@ -4,6 +4,7 @@ import com.teachmeskills.mycourse.coder.Coder;
 import com.teachmeskills.mycourse.controller.pars_document.FileParser;
 import com.teachmeskills.mycourse.controller.pars_document.Statistic;
 import com.teachmeskills.mycourse.exception.CheckNullSession;
+import com.teachmeskills.mycourse.exception.NullSummaryException;
 import com.teachmeskills.mycourse.service.SigInService;
 import com.teachmeskills.mycourse.session.Session;
 import com.teachmeskills.mycourse.util.Const;
@@ -11,7 +12,7 @@ import com.teachmeskills.mycourse.util.Const;
 import java.io.File;
 
 public class Runner {
-    public static void main(String[] args) throws CheckNullSession {
+    public static void main(String[] args) throws CheckNullSession, NullSummaryException {
         Session session = SigInService.sigIn("ivan_artur","root1");
         String path = "D:\\orders\\orders";
         Statistic.stats(FileParser.filePars(new File(path),session));
